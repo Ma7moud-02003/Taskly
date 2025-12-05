7c64718d445e022fabb699933bb98703132696c8
Ma7moud-02003 <MahmoudAmrTahoun2003@gmail.com>
2025-12-05 02:34:00 +0200

    enapling cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+ 
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
+   app.enableCors({
+  origin: '*',
+});
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true}));
 }
 bootstrap();
