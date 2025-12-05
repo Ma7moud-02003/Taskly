59e414ce915c0b93ae5f9b6ff6c434d83b6aba97
Ma7moud-02003 <MahmoudAmrTahoun2003@gmail.com>
2025-12-05 02:16:01 +0200

    fix PORT for railway

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT||3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true}));
 }
 bootstrap();
